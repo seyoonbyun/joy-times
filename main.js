841938dca76d59e6f0543af50a4495f3e0cba9a4
Seyoon Byun <syoon850@gmail.com>
2026-02-25 21:53:08 +0900

    Update API endpoint to Heroku backend

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const API_KEY = `939e4cb8554a4958a946476534952d50`;
+let news = [];
 const getLatestNews = async()=>{
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`);
     const response = await fetch(url);
     const data = await response.json();
-    console.log("rrrr" , response);
-    console.log("data", data);
+    news = data.articles;
+    console.log("data", news);
 };
 
 getLatestNews(); 
